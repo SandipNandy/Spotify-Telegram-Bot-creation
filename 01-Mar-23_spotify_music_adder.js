2ce417e70fb78be2e85f7c96959c7e5f0cbd906a
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-07-18 11:28:31 +0530

    Update 01-Mar-23_spotify_music_adder.js

--- 01-Mar-23_spotify_music_adder.js
+++ 01-Mar-23_spotify_music_adder.js
@@ -5,13 +5,13 @@
  * Spotify Telegram Bot Creation bot.
  *
  ***/
+
 // npm install spotify-web-api-node express cors open prompt-sync
 const SpotifyWebApi = require('spotify-web-api-node');
 const prompt = require('prompt-sync')();
 const express = require('express');
 const cors = require('cors');
 const open = require('open');
-
 const app = express();
 app.use(cors());
 let server = null;
