5fb3dc818b77513d62a89b027c509288487885f0
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-08-24 10:35:45 +0530

    Update 01-Mar-23_spotify_telegram_bot.js

--- 01-Mar-23_spotify_telegram_bot.js
+++ 01-Mar-23_spotify_telegram_bot.js
@@ -26,6 +26,7 @@ function checkUser(bot, msg) {
 
 async function setupTelegram(token) {
   // use polling unless you have a public server
+  
   const options = { polling: true };
 
   const bot = new TelegramBot(token, options);
