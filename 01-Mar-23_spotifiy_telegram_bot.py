16e7d3ff54bbb370df493703cf7d66ed4cc1285d
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-09-08 08:39:58 +0530

    Update 01-Mar-23_spotifiy_telegram_bot.py

--- 01-Mar-23_spotifiy_telegram_bot.py
+++ 01-Mar-23_spotifiy_telegram_bot.py
@@ -14,7 +14,6 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
 # states for the conversation as ENUM
 SEARCH, ADD_TRACK = range(2)
 
