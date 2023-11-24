a14785a82093275e09eee8279b9dd12b870b3c0e
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-11-24 09:14:23 +0530

    Update 01-Mar-23_spotify_telegram_bot.js

--- 01-Mar-23_spotify_telegram_bot.js
+++ 01-Mar-23_spotify_telegram_bot.js
@@ -13,7 +13,6 @@ const USERID = 'ENTER YOUR USERID HERE AS INTEGER';
 function checkUser(bot, msg) {
 
   const { id } = msg.chat;
-  
   if (id != USERID) {
     
     bot.sendMessage(id, 'You are not allowed to use this Bot!');
